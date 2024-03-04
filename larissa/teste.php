f1d2d52fe3092615a7f6419c04e52bbfb2c9b6e3
Larissa <95726877+laris-sa@users.noreply.github.com>
2024-03-04 15:08:58 -0300

    xampp

---
+++ larissa/teste.php
@@ -0,0 +1,5 @@
+<?php
+    //https://localhost/larissa/teste.php?nome=jose
+    echo "ola, " . $_GET["nome"]; //echo -> print "."=concatenaçao
+?>
+
---
+++ larissa/teste2.php
@@ -0,0 +1,4 @@
+<?php
+    //https://localhost/larissa/teste.php?nome=jose
+    echo "ola, " . $_GET["nome"]; //echo -> print "."=concatenaçao
+?>
